acee771071b8ed3336d835b4602fb7cb96e6837f
Alan França <alanfrancati@gmail.com>
2025-04-19 15:22:20 -0300

    Navigation ID name change

--- Inicial-page/src/scripts/javascriptHeader/scriptHeader.js
+++ Inicial-page/src/scripts/javascriptHeader/scriptHeader.js
@@ -3,7 +3,7 @@ const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
+    const nav = document.getElementById('nav-header');
     nav.classList.toggle('active');
     const active = nav.classList.contains('active');
     event.currentTarget.setAttribute('aria-expanded', active);
